e014983c45faf36a083e6dd59574a52ff595d285
Palakkushwani <palak.kushwani@gmail.com>
2021-08-25 15:16:54 +0530

    Initial commit

---
+++ loghandler/src/main/java/main/java/com/loghandler/model/DBEvent.java
@@ -0,0 +1,54 @@
+package main.java.com.loghandler.model;
+
+public class DBEvent {
+	
+	private String id;
+	private long duration;
+	private String type;
+	private String host;
+	private boolean alert;
+	
+	public DBEvent(String id, long duration, String type, String host, boolean alert) {
+		super();
+		this.id = id;
+		this.duration = duration;
+		this.type = type;
+		this.host = host;
+		this.alert = alert;
+	}
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public long getDuration() {
+		return duration;
+	}
+	public void setDuration(long duration) {
+		this.duration = duration;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getHost() {
+		return host;
+	}
+	public void setHost(String host) {
+		this.host = host;
+	}
+	public boolean isAlert() {
+		return alert;
+	}
+	public void setAlert(boolean alert) {
+		this.alert = alert;
+	}
+	
+	
+
+}
